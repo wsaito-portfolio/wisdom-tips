291cd33b9bd2cf792c2ef2cc9e0956e02bf37123
Wataru Saito <wsaito.jlo@gmail.com>
2020-05-01 13:11:33 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :microposts, dependent: :destroy
     attr_accessor :remember_token, :activation_token, :reset_token
     before_save { email.downcase!}
     before_create :create_activation_digest
@@ -59,6 +60,11 @@ class User < ApplicationRecord
     
     def password_reset_expired?
         reset_sent_at < 2.hours.ago
+    end
+    
+    # 試作feedの定義
+    def feed
+        Micropost.where("user_id=?", id)
     end
     
     private
