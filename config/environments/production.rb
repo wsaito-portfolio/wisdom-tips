07785e03799e0512830f7d3ea8386eb620c3dcc7
Wataru Saito <wsaito.jlo@gmail.com>
2020-05-21 13:07:28 +0000

    Update for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
   
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'murmuring-tundra-01087.herokuapp.com'
+  host = 'tips-shelf.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   
   ActionMailer::Base.smtp_settings = {
