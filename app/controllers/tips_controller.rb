145f7bd2c3d1ef71e964a08e609ff660c1587e52
Wataru Saito <wsaito.jlo@gmail.com>
2020-06-03 07:41:59 +0000

    create tip edit

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -9,19 +9,41 @@ class TipsController < ApplicationController
     
     def create
         @user = current_user
-        ActiveRecord::Base.transaction do
-            @tip = @user.tips.build(tip_params)
-            # @tip.user = current_user
-            @tip.save!
+        @tip = @user.tips.build(tip_params)
+        if @tip.save!
             flash[:success] = "tipを投稿しました。"
             redirect_to @user
-        rescue => e 
+        else
             render 'new'
         end
     end
     
     def destroy
     
+    end
+    
+    def update
+        @tip = Tip.find(params[:id])
+        @reason = @tip.reasons
+        @user = User.find(@tip.user_id)
+        if @tip.update_attributes(tip_params)
+          flash[:success] = "tipを更新しました。"
+          redirect_to @tip
+        else
+          render 'show'
+        end
+    end
+    
+    def show
+        @tip = Tip.find(params[:id])
+        @reason = @tip.reasons
+        @user = User.find(@tip.user_id)
+    end
+    
+    def edit
+        @tip = Tip.find(params[:id])
+        @reason = @tip.reasons
+        @user = User.find(@tip.user_id)
     end
     
     def tip_params
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,5 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations,only: [:edit]
   resources :password_resets, only:[:new,:create,:edit,:update]
-  resources :tips,only:[:new,:create,:destroy]
+  resources :tips
 end
