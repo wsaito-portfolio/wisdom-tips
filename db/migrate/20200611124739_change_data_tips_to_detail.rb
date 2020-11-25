a5514526ffed5b2dea55e179b88a671d1d02b1c6
Wataru Saito <wsaito.jlo@gmail.com>
2020-11-25 12:25:51 +0000

    migrate file fix

--- db/migrate/20200611124739_change_data_tips_to_detail.rb
+++ db/migrate/20200611124739_change_data_tips_to_detail.rb
@@ -1,5 +1,5 @@
 class ChangeDataTipsToDetail < ActiveRecord::Migration[5.1]
   def change
-      change_column :tips, :detail, :text
+      change_column :tips, :detail,'text USING CAST(detail AS text)'
   end
 end
--- db/migrate/20200612104854_change_data_type_tips_delete_flg.rb
+++ db/migrate/20200612104854_change_data_type_tips_delete_flg.rb
@@ -1,5 +1,5 @@
 class ChangeDataTypeTipsDeleteFlg < ActiveRecord::Migration[5.1]
   def change
-    change_column :tips, :delete_flg, :boolean, default: false, null: false
+    change_column :tips, :delete_flg, 'boolean USING CAST(delete_flg AS boolean)' , default: false, null: false
   end
 end
